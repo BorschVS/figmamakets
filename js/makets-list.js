5cee2108e011899bf0f5cbb858dde0dde60f2c63
BorschVS <borschvadym@gmail.com>
2023-09-26 00:43:25 +0300

    accordeon beta version

--- js/makets-list.js
+++ js/makets-list.js
@@ -1,18 +1,24 @@
 const list = document.querySelector(".projects-list");
 
 const listItem = document.querySelector(".projects-list__item");
-
 // const cartBox = document.querySelector(".cart-box");
 
 list.addEventListener("click", onListClick);
 function onListClick(evt) {
+  let activeEls = list.querySelectorAll(".cart-box.is-active");
+
+  activeEls.forEach((el) => {
+    if (el) {
+      el.classList.toggle("is-active");
+    }
+  });
+
   if (evt.target.classList.contains("projects-list__item")) {
     return;
   }
 
   const swatchEl = evt.target;
   const parentEl = swatchEl.closest(".projects-list__item");
-  console.log(parentEl);
   const childEl = parentEl.querySelector(".cart-box");
   childEl.classList.toggle("is-active");
 }
